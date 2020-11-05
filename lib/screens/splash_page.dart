66f6f777301d887bb4333b1fe85ece3a661caa20
phudb <phudb@lifetimetech.vn>
2020-11-05 14:58:05 +0700

    init commit

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirectPageByAuthenticate();
+  }
+
+  _redirectPageByAuthenticate() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token');
+
+    if (token != null) {
+      await Navigator.of(context).pushNamed('/users');
+    }
+    await Navigator.of(context).pushNamed('/login');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+              child: Container(
+                  child: Text('splash page',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50.0))),
+            )));
+  }
+}
