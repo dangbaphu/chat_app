ae972c81e4ce8414b81b4f7e0074859a5ed494a9
unknown <you@example.com>
2020-11-10 17:39:35 +0700

    login-cmt-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/splash_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:chat_app/blocs/auth_bloc/auth_bloc.dart';
+//import 'package:chat_app/blocs/home_bloc/home_bloc.dart';
+
+import 'blocs/simple_bloc_observer.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +15,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: SplashPage(),
-      routes: {
-      },
+      // home: SplashPage(),
+      // routes: {
+      // },
+      debugShowCheckedModeBanner: false,
+      title: 'Open Chat',
+      theme: ThemeData(
+          primaryColor: Color(0xfffae7e9),
+          accentColor: Color(0xfff2cbd0),
+          appBarTheme: AppBarTheme(
+            elevation: 0.0,
+            color: Colors.transparent,
+          )
+      ),
+      home: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthFailure) {
+            return LoginScreen();
+          }
+
+          if (state is AuthSuccess) {
+            return BlocProvider(
+              create: (context) => HomeBloc(),
+              child: HomeScreen(
+                user: state.user,
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
