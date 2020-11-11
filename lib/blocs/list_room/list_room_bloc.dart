e3cb9fe781993d5b26323f1561264c2f88c0390f
phu <baphu95@gmail.com>
2020-11-11 23:36:59 +0700

    phu commit

--- lib/blocs/list_room/list_room_bloc.dart
+++ lib/blocs/list_room/list_room_bloc.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:chat_app/blocs/list_room/list_room_event.dart';
 import 'package:chat_app/blocs/list_room/list_room_state.dart';
 import 'package:chat_app/services/list_rooms_service.dart';
-import 'package:meta/meta.dart';
 import 'package:chat_app/models/chat_room_info.dart';
 import 'dart:async';
 
@@ -10,14 +9,12 @@ class ListRoomBloc extends Bloc<ListRoomEvent, ListRoomState> {
 
   ListRoomBloc() : super(ListRoomInitial());
 
-  StreamSubscription listRooms;
 
   @override
   Stream<ListRoomState> mapEventToState(ListRoomEvent event) async* {
     if (event is ListRoomStart) {
       yield ListRoomLoading();
-      listRooms?.cancel();
-      listRooms = listRoomsService.getChatRooms().listen((rooms) {
+      listRoomsService.getChatRooms().listen((rooms) {
           add(ListRoomLoad(listRooms: rooms));
       });
     } else if (event is ListRoomLoad) {
